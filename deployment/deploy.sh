4fd3077450d4b4dc1e2b7a55a4ff6cd0d5cfc080
jstockwin <jstockwin@gmail.com>
2016-08-17 22:46:03 +0100

    Try using TRAVIS_REPO_SLUG env var

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,4 +2,4 @@
 
 echo "Deploying $TRAVIS_COMMIT"
 
-curl -NX POST http://139.59.176.9:3000/deploy --data "project=jakestockwin-co-uk/robin-thompson&commit=$TRAVIS_COMMIT&email=$EMAIL&password=$PASSWORD"
+curl -NX POST http://139.59.176.9:3000/deploy --data "project=TRAVIS_REPO_SLUG&commit=$TRAVIS_COMMIT&email=$EMAIL&password=$PASSWORD"
